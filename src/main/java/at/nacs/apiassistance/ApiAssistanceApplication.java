3bec2eae190c490ec86e1f9c6ea9c619462c5efc
5Rabie5 <n.rabietarboush@gmail.com>
2019-05-06 23:25:00 +0200

    project initialize

---
+++ src/main/java/at/nacs/apiassistance/ApiAssistanceApplication.java
@@ -0,0 +1,13 @@
+package at.nacs.apiassistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAssistanceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiAssistanceApplication.class, args);
+  }
+
+}
